a0b68f766da84b67368a9f56e642fa6ee0703ce0
Marcelo Magno Rodrigues <mmagnorodrigues@gmail.com>
2017-07-24 01:17:12 -0300

    OpenCV configured and tested.

---
+++ OpenCV3_Template/main.cpp
@@ -0,0 +1,40 @@
+#include "opencv2\opencv.hpp"
+#include <string>
+
+using namespace cv;
+
+int main(int argv, char** argc) {
+
+	std::cout << "Lenna 1" << std::endl;
+	
+	Mat test = imread("Lenna.png", CV_LOAD_IMAGE_UNCHANGED);
+
+	Mat clone = test.clone();
+
+	Point3_<uchar>* p;
+	int i, j;
+	
+	for (i = 0; i < clone.cols; i++) {
+		for (j = 0; j < clone.rows; j++) {
+			p = clone.ptr<Point3_<uchar> >(i, j);
+			ushort* r = (ushort*)&p->x;
+			ushort* g = (ushort*)&p->y;
+			ushort* b = (ushort*)&p->z;
+
+			/*std::cout << *r << std::endl;
+			std::cout << *g << std::endl;
+			std::cout << *b << std::endl;*/
+
+			*r = 0;
+			*g = 0;
+			*b = 0;
+
+			/*std::cout << *r << std::endl;
+			std::cout << *g << std::endl;
+			std::cout << *b << std::endl;*/
+		}
+	}
+	imshow("Lenna", clone);
+
+	waitKey();
+}
